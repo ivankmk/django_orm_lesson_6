b09f6b98baaab2991bb3d236b04957b36283c162
IvanKMK <kumeyko@hotmail.com>
2020-11-02 22:49:03 +0100

    Replace all sorted calls with order_by on main page

--- blog/views.py
+++ blog/views.py
@@ -33,7 +33,7 @@ def serialize_tag(tag):
 def index(request):
 
     posts = Post.objects.annotate(likes_count=Count('likes'))
-    popular_posts = posts.order_by('-likes_count')#sorted(posts, key=get_likes_count)
+    popular_posts = posts.order_by('-likes_count')
     most_popular_posts = popular_posts[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
@@ -41,8 +41,8 @@ def index(request):
 
     # tags = Tag.objects.all()
     tags = Tag.objects.annotate(related_posts_count=Count('posts'))#all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    popular_tags = tags.order_by('related_posts_count')#sorted(tags, key=get_related_posts_count)
+    most_popular_tags = popular_tags[:5]
 
     context = {
         'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
