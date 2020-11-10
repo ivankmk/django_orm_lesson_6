e6bea5a615738d81a03f1955909bb54e5095defe
IvanKMK <kumeyko@hotmail.com>
2020-11-10 23:14:44 +0100

    Optimize comments count evaluation

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,24 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 
+class PostQuerySet(models.QuerySet):
+    def year(self, year):
+        return self.filter(published_at__year=year).order_by('published_at')
+
+    def popular(self):
+        return self.annotate(likes_count=models.Count('likes')).order_by('-likes_count', 'title')
+
+    def fetch_with_comments_count(self):
+        posts_ids = [post.id for post in self]
+        posts_with_comments = Post.objects.filter(id__in=posts_ids).annotate(comments_count=models.Count('comments'))
+        ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+        count_for_id = dict(ids_and_comments)
+        for post in self:
+            post.comments_count = count_for_id[post.id]
+
+        return self
+
+
 class Post(models.Model):
     title = models.CharField("Заголовок", max_length=200)
     text = models.TextField("Текст")
@@ -14,6 +32,8 @@ class Post(models.Model):
     likes = models.ManyToManyField(User, related_name="liked_posts", verbose_name="Кто лайкнул", blank=True)
     tags = models.ManyToManyField("Tag", related_name="posts", verbose_name="Теги")
 
+    objects = PostQuerySet.as_manager()
+
     def __str__(self):
         return self.title
 
@@ -24,6 +44,11 @@ class Post(models.Model):
         ordering = ['-published_at']
         verbose_name = 'пост'
         verbose_name_plural = 'посты'
+
+
+class TagQuerySet(models.QuerySet):
+    def popular(self):
+        return self.annotate(posts_count=models.Count('posts')).order_by('-posts_count')
 
 
 class Tag(models.Model):
@@ -38,6 +63,8 @@ class Tag(models.Model):
     def get_absolute_url(self):
         return reverse('tag_filter', args={'tag_title': self.slug})
 
+    objects = TagQuerySet.as_manager() 
+
     class Meta:
         ordering = ["title"]
         verbose_name = 'тег'
@@ -45,7 +72,7 @@ class Tag(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey("Post", on_delete=models.CASCADE, verbose_name="Пост, к которому написан")
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, verbose_name="Пост, к которому написан", related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
 
     text = models.TextField("Текст комментария")
--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,8 @@ from django.db.models import Count
 
 
 def get_related_posts_count(tag):
-    return tag.related_posts_count
+    return tag.posts.count()
 
-def get_likes_count(post):
-    return post.likes_count
 
 def serialize_post(post):
     return {
@@ -15,6 +13,20 @@ def serialize_post(post):
         "teaser_text": post.text[:200],
         "author": post.author.username,
         "comments_amount": len(Comment.objects.filter(post=post)),
+        "image_url": post.image.url if post.image else None,
+        "published_at": post.published_at,
+        "slug": post.slug,
+        "tags": [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
+def serialize_post_optimized(post):
+    return {
+        "title": post.title,
+        "teaser_text": post.text[:200],
+        "author": post.author.username,
+        "comments_amount": post.comments_count,
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
@@ -31,29 +43,24 @@ def serialize_tag(tag):
 
 
 def index(request):
+    posts = Post.objects.prefetch_related('author')
 
-    posts = Post.objects.annotate(likes_count=Count('likes')).prefetch_related('author')
-    popular_posts = posts.order_by('-likes_count')
-    most_popular_posts = popular_posts[:5]
-
-    fresh_posts = Post.objects.order_by('published_at').prefetch_related('author')
-    most_fresh_posts = list(fresh_posts)[-5:]
-
-    # tags = Tag.objects.all()
-    tags = Tag.objects.annotate(related_posts_count=Count('posts'))#all()
-    popular_tags = tags.order_by('related_posts_count')#sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[:5]
+    most_popular_posts = posts.popular().fetch_with_comments_count()
+    most_fresh_posts = posts.annotate(comments_count=Count('comments')).order_by('-published_at', 'title')
+    most_popular_tags = Tag.objects.popular()
 
     context = {
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts[:5]],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts[:5]],
+        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags[:5]],
     }
     return render(request, 'index.html', context)
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    posts = Post.objects.prefetch_related('author')
+    post = posts.get(slug=slug)
+
     comments = Comment.objects.filter(post=post)
     serialized_comments = []
     for comment in comments:
@@ -79,16 +86,13 @@ def post_detail(request, slug):
         "tags": [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
-
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_tags = Tag.objects.popular()[:5]
+    most_popular_posts = posts.popular().fetch_with_comments_count()[:5]
 
     context = {
         'post': serialized_post,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
     }
     return render(request, 'post-details.html', context)
 
@@ -96,9 +100,7 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
